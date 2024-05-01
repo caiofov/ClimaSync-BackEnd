5fc1c82319a246f9c97863b8e6b2a926c12103cc
Caio Oliveira <cfoviana@alu.ufc.br>
2024-04-30 22:35:03 -0300

    remover arquivos

--- Climao/config.ts
+++
@@ -1,9 +0,0 @@
-import { TUYA_ACCESS_ID, TUYA_ACCESS_SECRET, DEVICE_ID } from "@env";
-
-const CONFIG = {
-  tuyaAccessID: TUYA_ACCESS_ID as string,
-  tuyaAccessSecret: TUYA_ACCESS_SECRET as string,
-  tuyaDeviceID: DEVICE_ID as string,
-};
-
-export default CONFIG;
