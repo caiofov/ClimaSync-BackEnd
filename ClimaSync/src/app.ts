b8157c8e2e4d3c9a624fc74535c2e1d4a1003962
Caio Oliveira <cfoviana@alu.ufc.br>
2024-05-16 15:05:51 -0300

    envs da tuya

--- ClimaSync/src/app.ts
+++ ClimaSync/src/app.ts
@@ -1,4 +1,5 @@
 import CONFIG from "./config";
+// Este arquivo deve conter todos as rotas da aplicação. É ele que é chamado quando esse projeto é rodado.
 
 const express = require("express");
 const app = express();
--- ClimaSync/src/config.ts
+++ ClimaSync/src/config.ts
@@ -1,7 +1,14 @@
 import { config as dotEnvConfig } from "dotenv";
+// Este arquivo configura as envs do arquivo .env.
+// Ele é útil para dar tipo ao valores das envs e organizar todas em um lugar
+
 dotEnvConfig();
 
 const CONFIG = {
   PORT: Number(process.env.SERVER_PORT),
+  TUYA_CLIENT_ID: process.env.TUYA_ACCESS_ID,
+  TUYA_CLIENT_SECRET: process.env.TUYA_ACCESS_SECRET,
+  TUYA_DEVICE_ID: process.env.TUYA_ACCESS_SECRET,
 };
+
 export default CONFIG;
