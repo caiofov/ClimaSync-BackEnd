bda373d6b12dd154c8b02d4e035d6d5ada1394c7
Caio Oliveira <cfoviana@alu.ufc.br>
2024-05-30 11:47:49 -0300

    endpoint para ligar e desligar tomada

--- ClimaSync/src/app.ts
+++ ClimaSync/src/app.ts
@@ -1,8 +1,6 @@
-import { stat } from "fs";
 import CONFIG from "./config";
-import { getTuyaStatus } from "./tuya";
+import { getTuyaStatus, sendTuyaCommand } from "./tuya";
 // Este arquivo deve conter todos as rotas da aplicação. É ele que é chamado quando esse projeto é rodado.
-
 const express = require("express");
 const app = express();
 
@@ -15,4 +13,16 @@ app.listen(CONFIG.PORT, () => {
 app.get("/tuya/status", async (req, res) => {
   const status = await getTuyaStatus();
   res.status(200).json(status);
+});
+
+app.post("/tuya/switch/:value", async (req, res) => {
+  if (!["true", "false"].includes(req.params.value)) {
+    res
+      .status(403)
+      .json(`Invalid value '${req.params.value}' - required 'true' or 'false'`);
+  } else {
+    const value: boolean = JSON.parse(req.params.value);
+    const commands = await sendTuyaCommand(value);
+    res.status(200).json(commands);
+  }
 });
