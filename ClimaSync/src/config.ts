52992912e6ef6535b6015871037614f652fc6e5a
Caio Oliveira <cfoviana@alu.ufc.br>
2024-06-19 17:45:09 -0300

    configurar envs

--- ClimaSync/src/config.ts
+++ ClimaSync/src/config.ts
@@ -9,6 +9,11 @@ const CONFIG = {
   TUYA_CLIENT_ID: process.env.TUYA_ACCESS_ID,
   TUYA_CLIENT_SECRET: process.env.TUYA_ACCESS_SECRET,
   WEATHER_KEY: process.env.WEATHER_KEY,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_PORT: Number(process.env.POSTGRES_PORT),
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
 };
 
 export default CONFIG;
