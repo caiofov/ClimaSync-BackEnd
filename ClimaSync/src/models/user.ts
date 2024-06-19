af7d67fc3c997dd5aff5299dd47aca90e0061ee6
Caio Oliveira <cfoviana@alu.ufc.br>
2024-06-19 17:45:17 -0300

    interface user

---
+++ ClimaSync/src/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  firebase_token: string;
+  device_id: string;
+  localizacao: string;
+  alerta_calor: boolean;
+  alerta_frio: boolean;
+  alerta_sol: boolean;
+  alerta_chuva: boolean;
+}
