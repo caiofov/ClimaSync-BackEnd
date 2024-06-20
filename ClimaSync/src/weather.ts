432370df7c0090c95bb9d3b6ed9d50526f126979
Caio Oliveira <cfoviana@alu.ufc.br>
2024-06-20 17:00:44 -0300

    requisição de clima pelo nome da cidade

--- ClimaSync/src/weather.ts
+++ ClimaSync/src/weather.ts
@@ -1,18 +1,31 @@
 import CONFIG from "./config";
 import { WeatherResponse } from "./models/weather";
+
 // faz as requisições para a API de clima
 
 const BASE_URL = "https://api.hgbrasil.com";
 
-export const getWeather = async (latitude: number, longitude: number) => {
-  const url =
-    `${BASE_URL}/weather?` +
-    new URLSearchParams({
-      key: CONFIG.WEATHER_KEY,
-      lat: latitude.toString(),
-      lon: longitude.toString(),
-    });
+//função geral de requisição
+const makeWeatherRequest = async (params: URLSearchParams) => {
+  params.append("key", CONFIG.WEATHER_KEY);
+  const url = `${BASE_URL}/weather?` + params;
+
   console.log(`Requesting ${url}`);
   const response: WeatherResponse = await fetch(url).then((r) => r.json());
   return response;
+};
+
+//busca as informações pelo nome da cidade
+export const getWeatherByName = async (name: string) => {
+  return makeWeatherRequest(new URLSearchParams({ city_name: name }));
+};
+
+//busca as informações pela coordenada
+export const getWeather = async (latitude: number, longitude: number) => {
+  return makeWeatherRequest(
+    new URLSearchParams({
+      lat: latitude.toString(),
+      lon: longitude.toString(),
+    })
+  );
 };
