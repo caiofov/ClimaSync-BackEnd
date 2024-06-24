63cb28c5466f22de3119d4c05ed58a23390fb452
luizaclara <lulu.lcap@gmail.com>
2024-06-24 00:42:58 -0300

    colocando chave privada em env

--- ClimaSync/src/alerts.ts
+++ ClimaSync/src/alerts.ts
@@ -1,3 +1,4 @@
+import CONFIG from "./config";
 import { getAllPlaces, listUsersByPlace } from "./dao";
 import { TEMP_ALERT, WEATHER_ALERT } from "./enums/weather";
 import { AlertType, User } from "./models/user";
@@ -7,7 +8,19 @@ import { getWeatherByName } from "./weather";
 
 // este arquivo contém as funções do cron job para coletar as informações de clima e notificar os usuários
 const admin = require('firebase-admin');
-const serviceAccount = require('.climasync-79c39-firebase-adminsdk-c2t60-5858a97c8d.json'); // Caminho para o arquivo de credenciais
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: CONFIG.FIREBASE_PROJECT_ID,
+  private_key_id: CONFIG.FIREBASE_PRIVATE_KEY_ID,
+  private_key: CONFIG.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: CONFIG.FIREBASE_CLIENT_EMAIL,
+  client_id: CONFIG.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: CONFIG.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
